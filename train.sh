36cf8ffabb607e28deb16b0afc834c4396338d37
kxz-thumt102-2 <15068701650@163.com>
2021-12-13 15:40:46 +0800

    add instruction and master address and port

--- train.sh
+++ train.sh
@@ -5,13 +5,35 @@
 # mail: 15068701650@163.com
 # Created Time: Saturday, December 11, 2021 PM04:49:45 HKT
 #########################################################################
-DATA_DIR=/data/private/kxz/prompt/webnlg_2017
+
 ########## adjust configs according to your needs ##########
+DATA_DIR=/data/private/kxz/prompt/webnlg_2017
 TRAIN_SET=${DATA_DIR}/train
 DEV_SET=${DATA_DIR}/dev
 SAVE_DIR=${DATA_DIR}/ckpt
 BATCH_SIZE=32
+######### end of adjust ##########
 
+########## Instruction ##########
+# This script takes three optional environment variables:
+# GPU / ADDR / PORT
+# e.g. Use gpu 0, 1 and 4 for training, set distributed training
+# master address and port to localhost:9901, the command is as follows:
+#
+# GPU="0,1,4" ADDR=localhost PORT=9901 bash train.sh
+#
+# Default value: GPU=-1 (use cpu only), ADDR=localhost, PORT=9901
+# Note that if your want to run multiple distributed training tasks,
+# either the addresses or ports should be different between
+# each pair of tasks.
+######### end of instruction ##########
+
+# set master address and port e.g. ADDR=localhost PORT=9901 bash train.sh
+MASTER_ADDR=localhost
+MASTER_PORT=9901
+if [ $ADDR ]; then MASTER_ADDR=$ADDR fi
+if [ $PORT ]; then MASTER_PORT=$PORT fi
+echo "Master address: ${MASTER_ADDR}, Master port: ${MASTER_PORT}"
 
 # set gpu, e.g. GPU="0,1,2,3" bash train.sh
 if [ -z "$GPU" ]; then
@@ -22,7 +44,7 @@ echo "Using GPUs: $GPU"
 GPU_ARR=(`echo $GPU | tr ',' ' '`)
 
 if [ ${#GPU_ARR[@]} -gt 1 ]; then
-    PREFIX="python -m torch.distributed.launch --nproc_per_node=${#GPU_ARR[@]}"
+	PREFIX="python -m torch.distributed.launch --nproc_per_node=${#GPU_ARR[@]} --master_addr=${MASTER_ADDR} --master_port=${MASTER_PORT}"
 else
     PREFIX="python"
 fi
