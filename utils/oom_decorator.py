72b015816cdc6a1e167fa1af05314f59a586e1be
kxz@thumt119-8 <jackie_kxz@outlook.com>
2023-11-30 18:43:18 +0800

    add backward oom

--- utils/oom_decorator.py
+++ utils/oom_decorator.py
@@ -18,9 +18,27 @@ def oom_decorator(forward):
             return output
         except RuntimeError as e:
             if 'out of memory' in str(e):
-                output = sum([p.norm() for p in self.parameters()]) * 0.0
+                output = sum([p.norm() for p in self.parameters() if p.dtype == torch.float]) * 0.0
                 return OOMReturn(output)
             else:
                 raise e
+    
     return deco_func
 
+
+def safe_backward(loss, model):
+    if isinstance(model, torch.nn.parallel.DistributedDataParallel):
+        loss.backward() # regrettedly, we cannot handle backward oom in distributed training
+        return True
+    
+    try:
+        loss.backward()
+        return True
+    except RuntimeError as e:
+        if 'out of memory' in str(e):
+            fake_loss = sum([p.norm() for p in model.parameters() if p.dtype == torch.float]) * 0.0
+            fake_loss.backward()
+            torch.cuda.empty_cache()
+            return False
+        else:
+            raise e
