47a559521ee576b7131b2772dc5689825f5e214e
kongxiangzhe <jackie_kxz@outlook.com>
2025-09-02 15:50:45 +0800

    add expand_like and sin-cos time embedding

--- utils/nn_utils.py
+++ utils/nn_utils.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
+import math
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -24,6 +26,11 @@ def sequential_or(*tensors):
     return res
 
 
+def expand_like(src, tgt):
+    src = src.reshape(*src.shape, *[1 for _ in tgt.shape[len(src.shape):]]) # [..., 1, 1, ...]
+    return src.expand_as(tgt)
+
+
 class SinusoidalPositionEmbedding(nn.Module):
     """
     Sin-Cos Positional Embedding
@@ -40,4 +47,24 @@ class SinusoidalPositionEmbedding(nn.Module):
         embeddings = torch.einsum('bn,d->bnd', position_ids, indices)
         embeddings = torch.stack([torch.sin(embeddings), torch.cos(embeddings)], dim=-1)
         embeddings = embeddings.reshape(-1, self.output_dim)
+        return embeddings
+
+
+class SinusoidalTimeEmbeddings(nn.Module):
+    '''
+        sin(1*t*2pi), sin(2*t*2pi), ...,
+        cos(1*t*2pi), cos(2*t*2pi)
+    '''
+    def __init__(self, dim):
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, time):
+        if self.dim == 1: # no projection
+            return time.reshape(-1,1)
+        device = time.device
+        half_dim = self.dim // 2
+        freq = 2 * torch.arange(half_dim, device=device) * math.pi
+        t = freq * time[..., None]
+        embeddings = torch.cat((t.sin(), t.cos()), dim=-1)
         return embeddings
